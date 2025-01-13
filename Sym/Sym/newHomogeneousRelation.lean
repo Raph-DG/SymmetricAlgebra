e5979642c48193aa1adbe760ede4d2abddced51a
pelicanhere <fakegreenall@foxmail.com>
2025-01-13 17:01:49 +0800

    Create newHomogeneousRelation.lean

---
+++ Sym/Sym/newHomogeneousRelation.lean
@@ -0,0 +1,55 @@
+import Mathlib
+--import LieAlgCohomology.MissingLemmas.TwoSidedIdeal
+
+/-
+The interaction between TwoSidedIdeal and Ideals seems to be not as good as we'd expected, and another PR involving reconstructing two-sided ideals is currently being reviewed at #17908. So, we decided not to mess with TwoSidedIdeal. Instead, we will try to state and prove the necessary things in homogeneous relations. (which actually made things easier to use for SymmetricAlgebra)
+-/
+
+#check RingQuot.mkRingHom
+#check RingQuot.mkAlgHom
+#check Relation.EqvGen
+
+variable {ι : Type*} [DecidableEq ι] [AddMonoid ι]
+variable {ι : Type*} [DecidableEq ι] [AddMonoid ι]
+variable {A : Type*} [Semiring A]
+
+class IsHomogeneousRelation {σ : Type*} [SetLike σ A] [AddSubmonoidClass σ A] (𝒜 : ι → σ) [GradedRing 𝒜]
+(r : A → A → Prop) : Prop where
+  is_homogeneous' : ∀ {x y : A}, (Relation.EqvGen r) x y →
+  ∀ i : ι, (Relation.EqvGen r) ((GradedRing.proj 𝒜 i) x) ((GradedRing.proj 𝒜 i) y)
+
+namespace HomogeneousRelation
+
+namespace HomogeneousRelation
+
+section RingCon
+
+variable {σ : Type*} [SetLike σ A] [AddSubmonoidClass σ A]
+variable (𝒜 : ι → σ) [GradedRing 𝒜] (rel : A → A → Prop) [inst : IsHomogeneousRelation 𝒜 rel]
+
+instance : IsHomogeneousRelation 𝒜 (RingQuot.Rel rel) := sorry
+instance : IsHomogeneousRelation 𝒜 (Relation.EqvGen rel) := sorry
+
+instance : IsHomogeneousRelation 𝒜 (RingConGen.Rel rel) :=
+  (RingQuot.eqvGen_rel_eq rel).symm ▸ (inferInstance)
+
+end RingCon
+
+section GradedRing
+
+variable (𝒜 : ι → AddSubmonoid A) [GradedRing 𝒜] (rel : A → A → Prop) [IsHomogeneousRelation 𝒜 rel]
+
+instance : GradedRing ((AddSubmonoid.map (RingQuot.mkRingHom rel)).comp 𝒜) := sorry
+
+end GradedRing
+
+section GradedAlgebra
+
+variable {R : Type*} [CommRing R] [Algebra R A]
+variable (𝒜 : ι → Submodule R A) [GradedAlgebra 𝒜] (rel : A → A → Prop) [IsHomogeneousRelation 𝒜 rel]
+
+instance : GradedAlgebra ((Submodule.map (RingQuot.mkAlgHom R rel)).comp 𝒜) := sorry
+
+end GradedAlgebra
+
+end HomogeneousRelation
